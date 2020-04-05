7b859a4f5b33800a3553bbefdfe255d9a39bc65d
francknoelkouassi <francknoel.kouassi@operantic.com>
2020-04-05 12:34:04 +0200

    first exercice ok

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1EnfantComponent implements OnInit {
 
-  constructor() { }
+  @Input() compteur: number;
+  @Output() changeValueCounter: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor() {}
 
   ngOnInit() {
+  }
+
+  incrementCounter(): void {
+    this.compteur++;
+    this.changeValueCounter.emit(this.compteur);
+  }
+
+  decrementCounter(): void {
+    this.compteur--;
+    this.changeValueCounter.emit(this.compteur);
   }
 
 }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1.component.css']
 })
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  public compteur: number;
+  public result: number;
 
-  constructor() { }
+  constructor() {
+    this.compteur = 0;
+  }
 
   ngOnInit() {
+  }
+
+  manageChange(event) {
+    this.result = event;
   }
 
 }
